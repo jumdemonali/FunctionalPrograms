78fbe29b26c4f9398d1ef849c49a136c1b8f7ba2
jumdemonali <jumdemonali@gmail.com>
2021-08-27 17:07:14 +0530

    Added: Roots Of equation

---
+++ src/main/java/com/bl/functions/Quadratic.java
@@ -0,0 +1,23 @@
+package com.bl.functions;
+import java.util.Scanner;
+
+public class Quadratic {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter value of a");
+        double a = scan.nextDouble();
+        System.out.println("Enter value of b");
+        double b = scan.nextDouble();
+        System.out.println("Enter value of c");
+        double c = scan.nextDouble();
+        double delta = (b*b-4*a*c);
+        System.out.println(delta);
+        double root1 = -b+(Math.sqrt(delta))/(2*a);
+        System.out.println(root1);
+        double root2 = -b-(Math.sqrt(delta))/(2*a);
+        System.out.println(root2);
+    }
+
+}
+
